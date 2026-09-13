5a2a2b07a556d83b180b428422dfd01f371b4d82
Islamul Haque <islamul0123@gmail.com>
2026-09-13 15:59:24 +0600

    host sever add

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,19 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+import os
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "wiscpulse-business-directory-3.onrender.com",
+]
+
+render_host = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+
+if render_host:
+    ALLOWED_HOSTS.append(render_host)
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
